0d13b0d2aa21ae0810f95221ba7e3ef59fd9412c
Gaurav Goswami <gaurav775.official@gmail.com>
2026-01-01 19:52:18 +0530

    api updated with live backend link

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://notes-resolver.onrender.com",
   withCredentials: true, // if you want cookies later
 });
 

b02fe543d5e4ef28ce458e9788e9eb3b99d92ee0
Gaurav775-Git <gaurav775.official@gmail.com>
2026-01-04 01:42:56 +0530

    cprogramming pdf files linked and data uploaded successfully

--- frontend/src/pages/subjects/sem1/Cprogram.jsx
+++ frontend/src/pages/subjects/sem1/Cprogram.jsx
@@ -20,11 +20,11 @@ const itemVariant = {
 
 const Cprogram = () => {
   const subjectinfo = [
-    { image: "", chaptername: "Unknown", unitnumber: "1", subjectcode: "CS-102" , link:"/sem1/cprogram/unit1"},
-    { image: "", chaptername: "Unknown", unitnumber: "2", subjectcode: "CS-102", link:"/sem1/cprogram/unit2"},
-    { image: "", chaptername: "Unknown", unitnumber: "3", subjectcode: "CS-102" , link:"/sem1/cprogram/unit3"},
-    { image: "", chaptername: "Unknown", unitnumber: "4", subjectcode: "CS-102" , link:"/sem1/cprogram/unit4"},
-    { image: "", chaptername: "Unknown", unitnumber: "5", subjectcode: "CS-102" , link:"/sem1/cprogram/unit5"},
+    { image: "", chaptername: "UNIT1", unitnumber: "1", subjectcode: "CS-102" , publicId: "notes/cprogramming/unit1"},
+    { image: "", chaptername: "UNIT2", unitnumber: "2", subjectcode: "CS-102", publicId: "notes/cprogramming/unit2"},
+    { image: "", chaptername: "UNIT3", unitnumber: "3", subjectcode: "CS-102" , publicId: "notes/cprogramming/unit3"},
+    { image: "", chaptername: "UNIT4", unitnumber: "4", subjectcode: "CS-102" , publicId: "notes/cprogramming/unit4"},
+    { image: "", chaptername: "UNIT5", unitnumber: "5", subjectcode: "CS-102" , publicId: "notes/cprogramming/unit5"},
   ];
 
   return (
@@ -58,7 +58,7 @@ const Cprogram = () => {
                 chaptername={item.chaptername}
                 unitnumber={item.unitnumber}
                 subjectcode={item.subjectcode}
-                link={item.link}
+                publicId={item.publicId}
               />
             </motion.div>
           ))}
